da38e0649f4880c5f84122bfb6100d2e49b25010
EFRA Mac <efrainroblespulido@gmail.com>
2024-10-11 08:48:18 -0600

    nav Dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -2,7 +2,16 @@ import React from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 
-function Dashboard(){
+function Dashboard() {
+    async function getUser() {
+        try {
+            const response = await axios.get('/user?ID=12345');
+            console.log(response);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     return (
         <div>
             holadd
