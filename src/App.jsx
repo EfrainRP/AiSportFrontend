db78ba20fa8f5192aa4693f7973a92e346ead2b4
EFRA Mac <efrainroblespulido@gmail.com>
2024-10-28 12:37:41 -0600

    working validation message

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 import Nav from './components/Inicio/Nav.jsx'
 import Welcome from './components/Inicio/Welcome.jsx'
-import SignUp from './components/Inicio/SignUp.jsx'
-import SignIn from "./components/Inicio/SignIn.jsx";
+import SignUp from './components/Inicio/SignUp/SignUp.jsx'
+import SignIn from "./components/Inicio/SignIn/SignIn.jsx";
 import Dashboard from "./components/Dashboard/Dashboard.jsx";
 
 function App(){
