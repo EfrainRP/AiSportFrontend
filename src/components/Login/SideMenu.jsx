8785e1878ae27887880388c918043e7b608d62f9
EfrainRP <efrain.robles5009@alumnos.udg.mx>
2024-12-22 00:08:16 -0600

    add props and ajust avatar icon

--- src/components/Login/SideMenu.jsx
+++ src/components/Login/SideMenu.jsx
@@ -44,7 +44,7 @@ const dataSideMenu = [ //TO DO: checar las urls
   {name: 'Equipos', img: <PeopleAltIcon/>, url: '#' },
   {name: 'Search', img: <SearchIcon/>, url: '#' },
   {name: 'Notifications', img: <ChatIcon/>, url: '#' },
-  {name: 'Profile', img: <Avatar {...stringAvatar('Kent Dodds')} sx={{transform:'scale(0.75)'}}/>, url: '#' }
+  {name: 'Profile', img: <Avatar {...stringAvatar('Kent Dodds')} sx={{width: 30, height: 30, fontSize:18}}/>, url: '#' } // TO DO: ingresar el name del usuario
 ];
 
 const openedMixin = (theme) => ({
@@ -77,29 +77,6 @@ const DrawerHeader = styled('div')(({ theme }) => ({
   ...theme.mixins.toolbar,
 }));
 
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
-})(({ theme }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(['width', 'margin'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  variants: [
-    {
-      props: ({ open }) => open,
-      style: {
-        marginLeft: drawerWidth,
-        width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(['width', 'margin'], {
-          easing: theme.transitions.easing.sharp,
-          duration: theme.transitions.duration.enteringScreen,
-        }),
-      },
-    },
-  ],
-}));
-
 const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
   ({ theme }) => ({
     width: drawerWidth,
@@ -128,20 +105,16 @@ const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' 
 function stringToColor(string) {
   let hash = 0;
   let i;
-
   /* eslint-disable no-bitwise */
   for (i = 0; i < string.length; i += 1) {
     hash = string.charCodeAt(i) + ((hash << 5) - hash);
   }
-
   let color = '#';
-
   for (i = 0; i < 3; i += 1) {
     const value = (hash >> (i * 8)) & 0xff;
     color += `00${value.toString(16)}`.slice(-2);
   }
   /* eslint-enable no-bitwise */
-
   return color;
 }
 
@@ -154,7 +127,7 @@ function stringAvatar(name) { // Funcion segun el nombre del usuario para el col
   };
 }
 
-export default function SideMenu() {
+export default function SideMenu(props) {
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
   const [selectedIndex, setSelectedIndex] = React.useState(0);
@@ -169,29 +142,7 @@ export default function SideMenu() {
   };
 
   return (
-    //   <AppBar position="fixed" open={open}>
-    //   <Toolbar>
-    //     <IconButton
-    //       color="inherit"
-    //       aria-label="open drawer"
-    //       onClick={handleDrawerOpen}
-    //       edge="start"
-    //       sx={[
-    //         {
-    //           marginRight: 5,
-    //         },
-    //         open && { display: 'none' },
-    //       ]}
-    //     >
-    //       <MenuIcon />
-    //     </IconButton>
-    //     <Typography variant="h6" noWrap component="div">
-    //       SpotHub
-    //     </Typography>
-    //   </Toolbar>
-    // </AppBar>
-    //<Box position="fixed" sx={{ display: { xs: 'flex', md: 'flex' }, gap: 1 }}>
-      <Drawer variant="permanent" open={open} sx={{display: 'block '}}>
+      <Drawer variant="permanent" open={open} sx={{display: 'block '}} {...props}>
         <DrawerHeader>
           <Box sx={open? { display: 'flex', flexGrow: 0.1, alignItems: 'center',} : { display:'none'}}>
             <SporthubIcon fontSize={{md:35,xs:40}}/>
@@ -215,7 +166,7 @@ export default function SideMenu() {
             <ListItem key={data.name} disablePadding 
               sx={[
                 { display: 'block' }, 
-                index==5? { mt: 'auto'} : {mt:0} // Si es el ultimo elemento
+                index==5? { mt: 'auto', alignItems: 'start'} : {} // Si es el ultimo elemento
               ]}
             >
               <ListItemButton
@@ -225,11 +176,11 @@ export default function SideMenu() {
                 selected={selectedIndex === index}
                 onClick={(event) => handleListItemClick(event, index)}
                 sx={[{
-                  minHeight: 75,
-                  px: 2.5,
-                },
-                open? { justifyContent: 'initial',} : { justifyContent: 'center',},
-              ]}>
+                    minHeight: 80,
+                    px: 2.5,
+                  },
+                  open? { justifyContent: 'initial',} : { justifyContent: 'center',},
+                ]}>
                 <ListItemIcon
                   sx={[{
                     minWidth: 0,
@@ -248,33 +199,6 @@ export default function SideMenu() {
             </ListItem>
           ))}
         </List>
-        {/* <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem key={text} disablePadding sx={{ display: 'block' }}>
-            <ListItemButton
-            sx={[{
-              minHeight: 48,
-              px: 2.5,
-              },
-              open? { justifyContent: 'initial', } : { justifyContent: 'center',},
-              ]}>
-              <ListItemIcon
-              sx={[{
-                minWidth: 0,
-                justifyContent: 'center',
-                },
-                open? {mr: 3, } : {mr: 'auto',},
-                ]}>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText
-                primary={text}
-                sx={[open? { opacity: 1, }: { opacity: 0, },]}
-                />
-                </ListItemButton>
-                </ListItem>
-                ))}
-                </List> */}
         <ColorModeSelect 
           transform={{xs:'scale(0.95)', md:'scale(0.75)'}} 
           sx={{
@@ -285,6 +209,5 @@ export default function SideMenu() {
           ml={9}
         />
       </Drawer>
-    //</Box>
   );
 }
