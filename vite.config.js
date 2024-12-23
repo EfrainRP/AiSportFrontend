e03b132877cbc28856bbc0742a1afae33780c23c
EfrainRP <efrain.robles5009@alumnos.udg.mx>
2024-12-22 18:45:46 -0600

    proxy config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.VITE_PROXY_SERVER, // Usar la variable de entorno
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), // Opcional: elimina '/api' del path
+      },
+    },
+  },
 })
