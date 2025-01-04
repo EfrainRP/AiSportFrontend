0c2de7f83a2a8656c4df46c31fde308e4f37fb98
EfrainRP <efrain.robles5009@alumnos.udg.mx>
2025-01-04 01:07:57 -0600

    adjust skeleton

--- src/views/Login/Tournaments/IndexTournament.jsx
+++ src/views/Login/Tournaments/IndexTournament.jsx
@@ -6,14 +6,18 @@ import {
     Card,
     CardActions,
     CardContent,
+    CardActionArea,
     ToggleButton,
     ToggleButtonGroup, 
     Button, 
-    Stack
+    Stack,
+    TextField ,
+    Autocomplete,
+    Fab  
 } from '@mui/material';
 import ViewListIcon from '@mui/icons-material/ViewList';
 import ViewModuleIcon from '@mui/icons-material/ViewModule';
-import ViewQuiltIcon from '@mui/icons-material/ViewQuilt';
+import AddIcon from '@mui/icons-material/Add';
 
 import axiosInstance from "../../../services/axiosConfig.js";
 import { useAuth } from '../../../services/AuthContext'; //  AuthContext
@@ -26,9 +30,10 @@ export default function IndexTournaments() {
     const { user, loading, setLoading } = useAuth(); // Accede al usuario autenticado 
     const [view, setView] = React.useState('list');
 
-  const handleChange = (event, nextView) => {
-    setView(nextView);
-  };
+    const handleChange = (event, nextView) => {
+        setView(nextView);
+    };
+    const [inputValue, setInputValue] = React.useState('');
 
     React.useEffect(() => { // Hace la solicitud al cargar la vista <-
         const fetchTorneos = async () => {
@@ -48,46 +53,70 @@ export default function IndexTournaments() {
 
     return (
         <LayoutLogin>
-            <ToggleButtonGroup
-                orientation="horizontal"
-                value={view}
-                exclusive
-                onChange={handleChange}
-                >
-                <ToggleButton value="list" aria-label="list">
-                    <ViewListIcon />
-                </ToggleButton>
-                <ToggleButton value="module" aria-label="module">
-                    <ViewModuleIcon />
-                </ToggleButton>
-            </ToggleButtonGroup>
+            <Typography variant='h2' sx={{ mb: 2 }}> {loading ? <Skeleton variant="rounded" width={'60%'} /> : `Welcome ${user.userName || 'invitado'} to you tournaments!`} </Typography>
+            {loading? 
+                <Box>
+                    <Skeleton variant="rounded" width={'50%'} height={40} sx={{my: 2}} />
+                    <Skeleton variant="circular" width={50} height={50} sx={{position: 'fixed', bottom: '3%', right: '3%'}}/>
+                </Box>
+            :
+                <Box>
+                    <Autocomplete
+                        freeSolo
+                        id="search-tournaments"
+                        disableClearable
+                        options={torneos.map((option) => option.name)}
+                        sx={{width:'50%', my: 3}}
+                        inputValue={inputValue}
+                        onInputChange={(event, newInputValue) => {
+                            setInputValue(newInputValue);
+                        }}
+                        renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                label="Search"
+                                slotProps={{
+                                input: {
+                                    ...params.InputProps,
+                                    type: 'search',
+                                    },
+                                }}
+                            />
+                        )}
+                    />
 
-            <Typography variant='h2' sx={{ mb: 2 }}> {loading ? <Skeleton /> : `Welcome ${user.userName || 'invitado'} to you tournaments!`} </Typography>
-            <h2><Link to="/torneo/create">Crear Torneo</Link></h2>
-            <Box sx={{ width: '100%', height: 'auto', mx: 2 }}>
-                {loading ? <Skeleton /> :
+                    <Fab color="primary" aria-label="add" size='medium' href='/torneo/create' sx={{position: 'fixed', bottom: '3%', right: '3%'}}>
+                        <AddIcon />
+                    </Fab>
+                </Box>
+            }
+
+            <Box sx={{ width: '100%', height: 'auto' }}>
+                {loading ? <Skeleton variant="rounded" width={'100%'} height={370} /> :
                     <Stack 
-                        spacing={{ xs: 1, sm: 2 }}
+                        spacing={{ xs: 1, sm: 1.5 }}
                         direction="row"
                         useFlexGap
                         sx={{ flexWrap: 'wrap' }}
                     >
                         {torneos.length > 0 ? (
                             torneos.map((torneo) => {
-                                return <Card variant="outlined" key={torneo.id}>
-                                    <CardContent>
+                                return (
+                                <Card variant="outlined" key={torneo.id} sx={{p:1}}>
+                                    <CardActionArea sx={{p:'6%', height:'100%'}}>
                                         <Typography gutterBottom variant="h5" component="div">
-                                        {torneo.name}
+                                            <strong>{torneo.name}</strong>
                                         </Typography>
-                                        <Typography>Ubicación: {torneo.ubicacion}</Typography>
-                                        <Typography>Descripción: {torneo.descripcion}</Typography>
-                                        <Typography>Fecha Inicio: {new Date(torneo.fechaInicio).toLocaleDateString()}</Typography>
-                                        <Typography>Fecha Fin: {new Date(torneo.fechaFin).toLocaleDateString()}</Typography>
-                                    </CardContent>
-                                    <CardActions>
+                                        <Typography><strong>Ubicación:</strong> {torneo.ubicacion}</Typography>
+                                        <Typography><strong>Descripción:</strong> {torneo.descripcion}</Typography>
+                                        <Typography><strong>Fecha Inicio:</strong> {new Date(torneo.fechaInicio).toLocaleDateString()}</Typography>
+                                        <Typography><strong>Fecha Fin:</strong> {new Date(torneo.fechaFin).toLocaleDateString()}</Typography>
+                                    </CardActionArea>
+                                    {/* <CardActions>
                                         <Button size="small" href={`/torneo/${torneo.name}/${torneo.id}`}>See</Button>
-                                    </CardActions>
+                                    </CardActions> */}
                                 </Card>
+                                );
                             }))
                             : (
                                 <Card variant="outlined">
@@ -98,7 +127,8 @@ export default function IndexTournaments() {
                                     </CardContent>
                                 </Card>
                             )}
-                    </Stack>}
+                    </Stack>
+                }
             </Box>
         </LayoutLogin>
     );
