7c165c90da618a0accaed770b55a14c3d2c24ae0
EfrainDextraLap <efrainroblespulido@gmail.com>
2025-04-28 11:01:51 -0600

    fixed errors debug

--- src/views/Basic/SignUp/SignUp.jsx
+++ src/views/Basic/SignUp/SignUp.jsx
@@ -45,7 +45,6 @@ const Card = styled(MuiCard)(({ theme }) => ({
   },
   boxShadow:
     'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
-    gap: theme.spacing(2),
   ...theme.applyStyles('dark', {
     boxShadow:
       'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px',
